43c4c2901be618cabd9d6d2472d2bfedafdf37d2
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-01-15 14:51:20 +0200

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+//1. Використовуючи функцію if...else,
+//напишіть код, який буде запитувати:
+//"What is the official name of JavaScript?"
+//Якщо користувач ввів "ECMAScript",
+//то показувати через alert: "True!"
+//у противному випадку показати:"Don't you know? ECMAScript!"
+
+//const ask = prompt('What is the official name of JavaScript?');
+
+// if (ask === 'ECMAScript') {
+//   alert('True!');
+// } else {
+//   alert("Don't you know? ECMAScript!");
+// }
+
+//ask === 'ECMAScript' ? alert('True!') : alert("Don't you know? ECMAScript!");
